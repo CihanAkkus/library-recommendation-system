72a736938028bf9237ac9364fa90091119b755ee
Cihan Akkuş <cihann.akkus2@gmail.com>
2026-01-02 22:49:41 +0300

    infrastructure.ts modified

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,20 +1,25 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib/core';
-import { InfrastructureStack } from '../lib/infrastructure-stack';
+import * as cdk from 'aws-cdk-lib';
+import { ApiStack } from '../lib/api-stack';
+import { DatabaseStack } from '../lib/database-stack';
+import { AuthStack } from '../lib/auth-stack';
 
 const app = new cdk.App();
-new InfrastructureStack(app, 'InfrastructureStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION || 'us-east-1',
+};
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const databaseStack = new DatabaseStack(app, 'LibraryDatabaseStack', { env });
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+const authStack = new AuthStack(app, 'LibraryAuthStack', { env });
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const apiStack = new ApiStack(app, 'LibraryApiStack', {
+  env,
+  booksTable: databaseStack.booksTable,
+  readingListsTable: databaseStack.readingListsTable,
+  userPool: authStack.userPool,
 });
+
+apiStack.addDependency(databaseStack);
+apiStack.addDependency(authStack);
